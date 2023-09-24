a66de338f38803bef44626923825e5021729ea49
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-09-23 18:44:34 -0700

    Create FilespaceVisualizer.tsx

---
+++ FilespaceVisualizer.tsx
@@ -0,0 +1,13 @@
+// components/FilespaceVisualizer.tsx
+import React from 'react';
+
+const FilespaceVisualizer: React.FC = () => {
+  return (
+    <div>
+      <h2>Filespace Visualizer</h2>
+      <textarea readOnly value="Placeholder for filespace" />
+    </div>
+  );
+};
+
+export default FilespaceVisualizer;
