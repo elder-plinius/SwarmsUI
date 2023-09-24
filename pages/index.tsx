95395d0794d2c5906c3b50212917e9ce6f7f63d2
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-09-23 19:17:37 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 // pages/index.tsx
+
 import React from 'react';
-import AgentInitializer from '../components/AgentInitializer';
+import AgentHierarchy from '../components/AgentHierarchy';
 import ApiKeyManager from '../components/ApiKeyManager';
 import Configuration from '../components/Configuration';
 import FilespaceVisualizer from '../components/FilespaceVisualizer';
@@ -10,11 +11,33 @@ const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Multi-Agent Autonomous AI System</h1>
-      <AgentInitializer />
-      <ApiKeyManager />
-      <Configuration />
-      <FilespaceVisualizer />
-      <AgentTaskVisualizer />
+      
+      <div>
+        <h2>Agent Initialization</h2>
+        <AgentHierarchy />
+      </div>
+      
+      <div>
+        <h2>API Key Manager</h2>
+        <ApiKeyManager />
+      </div>
+
+      <div>
+        <h2>Configuration</h2>
+        <Configuration />
+      </div>
+      
+      <div>
+        <h2>Filespace Visualizer</h2>
+        <FilespaceVisualizer />
+      </div>
+
+      <div>
+        <h2>Agent Task Visualizer</h2>
+        <AgentTaskVisualizer />
+      </div>
+
+      {/* Add other components and layout here as needed */}
     </div>
   );
 };
